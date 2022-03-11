f855089881a455db2d54da89b2f48680006fc4e8
jafeth montero naranjo <jafethmonteronaranjo@jafeths-MBP.bbrouter>
2022-03-11 14:52:15 -0600

    setting route on backend

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,68 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
-  const body = req.body;
-  const newUser = new User(body);
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
     const user = await newUser.save();
+
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong User Name");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong Password");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const updateUser = async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            }, { new: true });
+
+            const { password, ...others } = updatedUser._doc;
+            res.status(200).json(others);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+    } else {
+        res.status(401).json("You can not update this account")
+    };
+  };
+
+  export const getUser = async (req, res) => {
+      try {
+         const user = await User.findById(req.params.id);
+         const { password, ...others } = user._doc;
+
+         res.status(200).json(others);
+      } catch (error) {
+          res.status(500).json(error);
+      }
+  }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { register } from '../controllers/users.js';
+import { register, login, updateUser, getUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-router.post('/register', register)
+router.post('/register', register);
+router.post('/login', login);
+router.put('/:id', updateUser);
+router.get('/:id', getUser);
 
 export default router; 
